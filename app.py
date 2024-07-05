5e9fed9c05503bb16011baa9ffc0fad28afb0887
SWPNL <sd2269@njit.edu>
2024-07-05 01:51:46 -0400

    initial code

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
 from dotenv import load_dotenv
+import llama_index
+
 
 load_dotenv()
 
+os.environ = os.getenv("OPENAI_API_KEY")
+
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+documents = SimpleDirectoryReader("data").load_data()
